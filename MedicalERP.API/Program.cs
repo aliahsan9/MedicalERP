51e1b0c5a192ce1d57ef8175fb2888b32671c9de
Ali Ahsan <aliahsan8751@gmail.com>
2026-06-05 21:47:31 +0500

    updated controller

--- MedicalERP.API/Program.cs
+++ MedicalERP.API/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                "http://localhost:4200",   // Angular dev
+                "http://localhost:4200",   
                 "https://localhost:4200"
             )
             .AllowAnyHeader()
